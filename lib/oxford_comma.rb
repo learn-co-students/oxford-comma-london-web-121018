86dba1fc977283748b7e3d8a254a20f816ae3094
georgejeffery <georgejeffery@gmail.com>
2018-11-29 14:12:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+if array.length==1
+  array.join
+elsif array.length ==2
+  array.join(" and ")
+else
+  i=0
+  newstring = ""
+  while (i<=(array.length-2)) do
+    newstring += "#{array[i]}, "
+    i +=1
+  end
+  newstring += "and #{array.last}"
+end
 end
