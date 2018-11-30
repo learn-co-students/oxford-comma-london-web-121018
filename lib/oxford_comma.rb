7dff1d910705fbd5b52e34084daf03445bb0a83a
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-30 06:17:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    return array.join(" and ")
+  else array.length >= 3
+    last_element= array.pop
+    string = array.join(", ")
+    string +=", and #{last_element}"
+    # array[-1] = " and #{array[-1]}"
+    return string
+ end
 end
