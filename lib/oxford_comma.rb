ec22e296d6066e6bfbac3364219af71831b3b27a
ruthnewman <ruthnewman@gmail.com>
2018-11-30 14:18:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(array)#
+  if array.count == 1 
+    array[0]
+  elsif array.count == 2 
+    array.join(" and ")
+  elsif array.count >= 2 
+    final_item = array.pop
+    new_string = array.join(", ")
+    newest_string = "#{new_string}, and #{final_item}"
+  end 
 end
