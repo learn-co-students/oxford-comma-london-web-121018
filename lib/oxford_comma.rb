61f10778ee849157c685116a57fa4830a226d53a
I_AM Elaine <elaine.onb@gmail.com>
2018-12-06 20:36:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2 
+    return array.join(" and ")
+  else 
+    str = "#{array.pop()}"
+    array.push("and ")
+    array.join(", ") + str
+  end
 end
